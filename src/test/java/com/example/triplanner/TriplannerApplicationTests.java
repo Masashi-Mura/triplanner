8d181ea8debde4dd732d65e5222d44c1db14c75b
Masashi <k7007674@gmail.com>
2024-01-19 10:51:32 +0900

    first commit

---
+++ src/test/java/com/example/triplanner/TriplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.triplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
