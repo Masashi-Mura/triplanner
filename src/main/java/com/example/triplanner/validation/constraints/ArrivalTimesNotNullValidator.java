5e3f882009875bae84b2878823126ab075a7ba9d
Masashi <k7007674@gmail.com>
2024-02-23 08:16:37 +0900

    Validato change for ArrivalTimes and PlaceNames#34

--- src/main/java/com/example/triplanner/validation/constraints/ArrivalTimesNotNullValidator.java
+++ src/main/java/com/example/triplanner/validation/constraints/ArrivalTimesNotNullValidator.java
@@ -14,12 +14,14 @@ public class ArrivalTimesNotNullValidator implements ConstraintValidator<Arrival
 	
 	@Override
 	public boolean isValid(List<LocalDateTime> arrivalTimes, ConstraintValidatorContext context) {
-		if (arrivalTimes.size() == 0) {
+		if (arrivalTimes == null) {
+			return true;//確認画面から戻る時にtrue。場所が1カ所のみもtrueだが場所の個数のValidateでfalseになる。
+		} else if (arrivalTimes.size() == 0) {
 			return false;
-		} else if (arrivalTimes.get(0) == null) {
-			return false;
+		} else if (arrivalTimes.get(0) != null) {
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 }
--- src/main/java/com/example/triplanner/validation/constraints/PlaceNamesLengthValidator.java
+++ src/main/java/com/example/triplanner/validation/constraints/PlaceNamesLengthValidator.java
@@ -5,21 +5,23 @@ import java.util.List;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class PlaceNamesLengthValidator implements ConstraintValidator<PlaceNamesLength, List<String>>{
-	
+public class PlaceNamesLengthValidator implements ConstraintValidator<PlaceNamesLength, List<String>> {
+
 	int min;
-	
+
 	@Override
 	public void initialize(PlaceNamesLength annotation) {
 		this.min = annotation.min();
 	}
-	
+
 	@Override
 	public boolean isValid(List<String> placeNames, ConstraintValidatorContext context) {
-		if (placeNames.size() < this.min) {
-			return false;
+		if (placeNames == null) {
+			return true;
+		} else if (placeNames.size() >= this.min) {
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 }
