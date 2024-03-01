4b389be53492dda60e77dc7db2a40cf9c28085e8
Masashi <k7007674@gmail.com>
2024-03-01 21:15:19 +0900

    Created jar file for deployment#49

--- src/main/java/com/example/triplanner/TriplannerApplication.java
+++ src/main/java/com/example/triplanner/TriplannerApplication.java
@@ -2,12 +2,19 @@ package com.example.triplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TriplannerApplication {
+public class TriplannerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TriplannerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TriplannerApplication.class);
 	}
 
 }
