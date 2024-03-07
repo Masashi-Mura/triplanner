48a3ed5dc35dbe8da523fb518c20e038edc5e5f2
Masashi <k7007674@gmail.com>
2024-03-07 16:44:19 +0900

    Crated pagination for tripsIndex#38

--- src/main/java/com/example/triplanner/repository/TripsRepository.java
+++ src/main/java/com/example/triplanner/repository/TripsRepository.java
@@ -2,6 +2,8 @@ package com.example.triplanner.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.triplanner.entity.Trips;
@@ -10,4 +12,5 @@ public interface TripsRepository extends JpaRepository<Trips, Integer> {
 
 	List<Trips> findAllByOrderByUpdatedAtDesc();//開発用のため公開設定を無視。　実際は公開のみを取得。
 	
+	Page<Trips> findAllByOrderByUpdatedAtDesc(Pageable pageable);
 }
